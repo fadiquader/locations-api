461a528a5fe231015489304b8eea8bd3ee8f5472
fadi quader <fadi.quader@gmail.com>
2018-11-30 19:20:27 +0200

    add protected route middleware

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,6 @@ import { protectRoute } from "../middlewares";
 const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
-router.post('/me', protectRoute, authController.me);
+router.get('/me', protectRoute, authController.me);
 
 export default router;
